8e61032dc389618bd21917f67f000286cba3e35f
Caryl <caryl830@gmail.com>
2021-02-27 01:28:26 +0800

    Set up database connection

---
+++ routes.js
@@ -0,0 +1,4 @@
+let router = require('express').Router();
+let userController = require('./controllers/userController');
+
+ module.exports = router;
--- server.js
+++ server.js
@@ -1,102 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const MongoClient = require('mongodb').MongoClient
-
-const conString = 'mongodb+srv://master-yoda:ketamine@cluster0.37jpr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
-// Set ejs engine
-app.set('view engine', 'ejs')
+const apiRoutes = require('./routes');
+const mongoose = require('mongoose');
 
 /**
  *  Middlewares
  */
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static('public'))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
-// Create server for browser to connect to
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+// Use API routes
+app.use('api/', apiRoutes);
+
+/**
+ * Constants
+ */
+let port = process.env.PORT || 3000;
+let conString = 'mongodb+srv://carrot:LUeoQCrd4JIJhg0G@cluster0.jsegz.mongodb.net/singhealth-backend?retryWrites=true&w=majority'; // TODO: Set connection string as environment variable
+let options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+// Launch app
+app.listen(port, () => {
+    console.log("Running app on port " + port);
 })
 
 // Connect to MongoDB
-// Using promises instead of callbacks
-MongoClient.connect(conString, {useUnifiedTopology: true})
-    .then(client => {
-        console.log('Connected to Database')
+mongoose.connect(conString, options)
+    .then(_ => {
+        console.log("Connected to database");
+    }, err => {
+        console.error(err);
+    });
 
-        // Change the database
-        const db = client.db('star-wars-quotes')
+/**
+ * Default API response - for development only!
+ */
+app.get('/', (req, res) => {
+    res.json({
+       not_obi_wan: "Hello there",
+       not_grievous: "GENERAL KENOBI"
+    });
+});
 
-        // Create a collection
-        const quotesCollection = db.collection('quotes')
 
-        /**
-         *  GET REQUESTS
-         */
-        app.get('/', (req, res) => {
-            db.collection('quotes').find().toArray()
-                .then(results => {
-                    res.render('index.ejs', { quotes: results})
-                    console.log(results) // for debugging
-                })
-                .catch(error => console.error(error))
-            
-        })
 
-        /**
-         *  POST REQUESTS
-         */
-        app.post('/quotes', (req, res) => {
-            quotesCollection.insertOne(req.body)
-                .then(result => {
-                    console.log(result)
-
-                    // Redirect back to form
-                    res.redirect('/')
-                })
-                .catch(error => console.error(error))
-        })
-
-        /**
-         *  PUT REQUESTS
-         */
-        app.put('/quotes', (req, res) => {
-            quotesCollection.findOneAndUpdate(
-                { name: 'Yoda'},
-                {
-                    $set: {
-                        name: req.body.name,
-                        quote: req.body.quote
-                    }
-                },
-                {
-                    upsert: true
-                }
-            )
-                .then(_ => {
-                    res.json('Success') // Send back to request
-                })
-                .catch(error => console.error(error))
-        })
-
-        /**
-         *  DELETE REQUESTS
-         */
-        app.delete('/quotes', (req, res) => {
-            quotesCollection.deleteOne(
-                { name: req.body.name }
-            )
-                .then(result => {
-                    if (result.deletedCount === 0) {
-                        return res.json('No Anakin quotes left to delete')
-                    }
-                    res.json('Bye Ani')
-                })
-                .catch(err => console.error(err))
-        })
-
-    })
-    .catch(error => console.error(error))
 
