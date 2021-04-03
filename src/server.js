41353056272c9b06f0c5131c5d70e76caa0db348
Caryl <caryl830@gmail.com>
2021-04-03 16:29:08 +0800

    Remove node_modules folder

--- src/server.js
+++ src/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-/**
- *  Middlewares
- */
+// Middlewares
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
