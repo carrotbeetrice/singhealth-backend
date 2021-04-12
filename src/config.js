eb64586db14ccc78249370c70c882e17e4c8478a
carrot <caryl830@gmail.com>
2021-04-13 01:50:30 +0800

    Finish export to excel test

--- src/config.js
+++ src/config.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 
 module.exports = {
-    SECRET: process.env.JWT_SECRET,
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    REGION: process.env.REGION,
-    BUCKET_NAME: process.env.BUCKET_NAME
+    aws: {
+        SECRET: process.env.JWT_SECRET,
+        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+        REGION: process.env.REGION,
+        BUCKET_NAME: process.env.BUCKET_NAME,
+        SIGNATURE_VERSION: "v4",
+    },
+    email: {
+        MAIL_USERNAME: process.env.MAIL_USERNAME,
+        MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+        OAUTH_CLIENTID: process.env.OAUTH_CLIENTID,
+        OAUTH_CLIENT_SECRET: process.env.OAUTH_CLIENT_SECRET,
+        OAUTH_REFRESH_TOKEN: process.env.OAUTH_REFRESH_TOKEN,
+    },
 };
